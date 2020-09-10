ac6fd9406510561f432e153ed68d07b869c44c83
Jenny <jelusita11@gmail.com>
2020-09-10 08:40:33 -0500

    adding the prediction endpoint

--- packages/ml_api/tests/test_controller.py
+++ packages/ml_api/tests/test_controller.py
@@ -5,6 +5,8 @@ from regression_model.processing.data_management import load_dataset
 import json
 import math
 
+from api import __version__ as api_version
+
 
 def test_health_endpoint_returns_200(flask_test_client):
     # When
@@ -14,6 +16,15 @@ def test_health_endpoint_returns_200(flask_test_client):
     assert response.status_code == 200
 
 
+def test_version_endpoint_returns_version(flask_test_client):
+    # When
+    response = flask_test_client.get('/version')
+
+    # Then
+    assert response.status_code == 200
+    response_json = json.loads(response.data)
+    assert response_json['model_version'] == _version
+    assert response_json['api_version'] == api_version
 def test_prediction_endpoint_returns_prediction(flask_test_client):
     # Given
     # Load the test data from the regression_model package
