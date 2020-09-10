22d5d33a8aaf5eac0caf51a3d0bf8416d13332e6
Jenny <jelusita11@gmail.com>
2020-09-10 01:54:44 -0500

    adding the prediction endpoint

--- packages/ml_api/api/controller.py
+++ packages/ml_api/api/controller.py
@@ -1,4 +1,5 @@
 from flask import Blueprint, request
+from regression_model.predict import make_prediction
 
 from api.config import get_logger
 
@@ -13,3 +14,27 @@ def health():
     if request.method == 'GET':
         _logger.info('health status OK')
         return 'ok'
+
+
+@prediction_app.route('/v1/predict/regression', methods=['POST'])
+def predict():
+    if request.method == 'POST':
+        # Step 1: Extract POST data from request body as JSON
+        json_data = request.get_json()
+        _logger.debug(f'Inputs: {json_data}')
+
+        # Step 2: Validate the input using marshmallow schema
+        input_data, errors = validate_inputs(input_data=json_data)
+
+        # Step 3: Model prediction
+        result = make_prediction(input_data=input_data)
+        _logger.debug(f'Outputs: {result}')
+
+        # Step 4: Convert numpy ndarray to list
+        predictions = result.get('predictions').tolist()
+        version = result.get('version')
+
+        # Step 5: Return the response as JSON
+        return jsonify({'predictions': predictions,
+                        'version': version,
+                        'errors': errors})
